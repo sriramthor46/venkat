9cd11400089472b5b0115c37688d7d711934d5b6
sriramthor46 <56017955+sriramthor46@users.noreply.github.com>
2019-10-03 12:39:28 +0530

    Update FirstClass.java
    sub method added

--- sriram/src/com/icic/loans/carloans/houseloans/FirstClass.java
+++ sriram/src/com/icic/loans/carloans/houseloans/FirstClass.java
@@ -9,6 +9,12 @@ public void add()
 	c=a+b;
 	
 }
+
+public void sub()
+{
+	int a,b,c;
+	c=a-b;
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is my first program");
