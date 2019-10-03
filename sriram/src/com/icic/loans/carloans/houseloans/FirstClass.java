b8581796d3a8afebbbd57e1cb01ca7d54330292d
sriramthor46 <56017955+sriramthor46@users.noreply.github.com>
2019-10-03 12:38:20 +0530

    Update FirstClass.java
    no changes

--- sriram/src/com/icic/loans/carloans/houseloans/FirstClass.java
+++ sriram/src/com/icic/loans/carloans/houseloans/FirstClass.java
@@ -10,12 +10,7 @@ public class FirstClass {
 	d=a+b;
 	
 }
-	public void sub()
-	{
-		int a b,c;
-		c=a-b;
-	}
-
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
